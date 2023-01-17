1fbc687438eb89fc34e347c502528412e0054499
mpnogaj <mpnogaj@gmail.com>
2023-01-17 03:04:53 +0100

    Added documentation for graph class

--- graph.h
+++ graph.h
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+/// @brief Klasa przechowujaca graf
 class Graph
 {
 private:
@@ -13,8 +14,34 @@ private:
 public:
 	Graph();
 	~Graph();
+
+	/**
+	 * @brief Funkcja dodajaca krawedz pomiedzy dwoma wierzcholkami 
+	 * 
+	 * @param a pierwszy wierzcholek
+	 * @param b drugi wierzcholek
+	 */
 	void AddEdge(int a, int b);
+
+	/**
+	 * @brief Funkcja sprawdzajaca dwudzielnosc grafu
+	 * 
+	 * @return true jezeli graf jest dwudzielny
+	 * @return false jezeli graf nie jest dwudzielny
+	 */
 	bool IsBigraph();
+
+	/**
+	 * @brief Wypisz graf na strumien wyjsciowy
+	 * 
+	 * @param outputStream strumien wyjsciowy na ktory nalezy wypisac graf
+	 */
 	void PrintGraph(ostream& outputStream);
+
+	/**
+	 * @brief Wypisz 2 zbiory wierzcholkow na strumien wyjsciowy. Powinna byc uzywana tylko po sprawdzeniu dwudzielnosci funkcja @ref IsBigraph()
+	 * 
+	 * @param outputStream strumien wyjsciowy na ktory nalezy wypisac graf
+	 */
 	void PrintSets(ostream& outputStream);
 };
